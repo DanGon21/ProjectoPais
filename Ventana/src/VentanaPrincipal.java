f5f696127729bda38410e6c099f97764b316d6ba
ausias <ausias@hINFO6-27.xiam.cat>
2023-05-24 13:04:22 +0200

    Programa acabado(buscar fallos)

--- Ventana/src/VentanaPrincipal.java
+++ Ventana/src/VentanaPrincipal.java
@@ -73,7 +73,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         buttonDelete = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         listaCiudades = new javax.swing.JList<>();
-        jLabel1 = new javax.swing.JLabel();
+        ListaPaises = new javax.swing.JLabel();
         buttonCerrar = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         poblacio = new javax.swing.JSpinner();
@@ -136,7 +136,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         listaCiudades.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         jScrollPane2.setViewportView(listaCiudades);
 
-        jLabel1.setText("Lista Paises");
+        ListaPaises.setText("Lista Paises");
 
         buttonCerrar.setText("CERRAR");
         buttonCerrar.addActionListener(new java.awt.event.ActionListener() {
@@ -145,7 +145,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             }
         });
 
-        jLabel2.setText("Lista Ciudades ");
+        jLabel2.setText("ListaCiudades");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -171,7 +171,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                         .addGap(77, 77, 77)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(buttonEdit, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(ListaPaises, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(buttonDelete, javax.swing.GroupLayout.Alignment.TRAILING)))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(afegir)
@@ -199,7 +199,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(codi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1)
+                            .addComponent(ListaPaises)
                             .addComponent(Codi))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -419,6 +419,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Codi;
     private javax.swing.JLabel Europa;
+    private javax.swing.JLabel ListaPaises;
     private javax.swing.JLabel Nom;
     private javax.swing.JLabel Poblacio;
     private javax.swing.JButton afegir;
@@ -427,7 +428,6 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton buttonEdit;
     private javax.swing.JTextField codi;
     private javax.swing.JRadioButton europa;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
